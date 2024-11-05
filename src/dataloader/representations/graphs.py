5a23c12c85eaf8f03e0d4915b9636e54d6f9f1c8
beangoben <beangoben@gmail.com>
2024-11-05 15:48:19 -0500

    formatting

--- src/dataloader/representations/graphs.py
+++ src/dataloader/representations/graphs.py
@@ -31,25 +31,6 @@ def graphein_molecular_graphs(
     return [gm.construct_graph(smiles=i, config=graphein_config) for i in smiles]
 
 
-def graphein_molecular_graphs(
-    smiles: List[str], graphein_config: Optional[bool] = None
-) -> List[nx.Graph]:
-    """
-    Convers a list of SMILES strings into molecular graphs
-    using the feautrisation utilities of graphein.
-
-    :param smiles: list of molecular SMILES
-    :type smiles: list
-    :param graphein_config: graphein configuration object
-    :type graphein_config: graphein/config/graphein_config
-    :return: list of molecular graphs
-    """
-
-    import graphein.molecule as gm
-
-    return [gm.construct_graph(smiles=i, config=graphein_config) for i in smiles]
-
-
 def pyg_molecular_graphs(
     smiles: List[str],
 ) -> List[Data]:
